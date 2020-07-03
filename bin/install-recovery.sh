bb952af7da690995b735f0ad95b32932a7a02eb5
Anand Shekhawat <anandsingh215@yahoo.com>
2020-07-03 07:58:04 +0530

    miui_HMNote5_20.6.18_887af1fbe4_9.0.zip

--- bin/install-recovery.sh
+++ bin/install-recovery.sh
@@ -1,6 +1,6 @@
 #!/system/bin/sh
-if ! applypatch -c EMMC:/dev/block/bootdevice/by-name/recovery:34383182:963722f08aec291eabd35a7e19c143672555b55d; then
-  applypatch -b /system/etc/recovery-resource.dat EMMC:/dev/block/bootdevice/by-name/boot:27743562:44d07a25893ac8bc35b366358ec130cf87e76622 EMMC:/dev/block/bootdevice/by-name/recovery 963722f08aec291eabd35a7e19c143672555b55d 34383182 44d07a25893ac8bc35b366358ec130cf87e76622:/system/recovery-from-boot.p && log -t recovery "Installing new recovery image: succeeded" || log -t recovery "Installing new recovery image: failed"
+if ! applypatch -c EMMC:/dev/block/bootdevice/by-name/recovery:34383182:21811c7e9bf11e930b352925dc3921df4092b564; then
+  applypatch -b /system/etc/recovery-resource.dat EMMC:/dev/block/bootdevice/by-name/boot:27743562:687981dad9661eeab38594ba6746ab770ff85a58 EMMC:/dev/block/bootdevice/by-name/recovery 21811c7e9bf11e930b352925dc3921df4092b564 34383182 687981dad9661eeab38594ba6746ab770ff85a58:/system/recovery-from-boot.p && log -t recovery "Installing new recovery image: succeeded" || log -t recovery "Installing new recovery image: failed"
 else
   log -t recovery "Recovery image already installed"
 fi
